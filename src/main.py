6ac63ac7b6a552bdffbd8359c6c80428711b07d2
aneerati <aneerati2004@gmail.com>
2023-09-12 11:38:26 -0400

    playing

--- src/main.py
+++ src/main.py
@@ -2,8 +2,7 @@ import bs4
 
 with open("index.html", 'r') as html_file:
     content = html_file.read()
+    print(content)
 
     soup = bs4.BeautifulSoup(content, 'lxml')
-    course_html_tags = soup.find_all('h5')
-    for course in course_html_tags:
-        print(course.text)
+    print(soup.prettify())
