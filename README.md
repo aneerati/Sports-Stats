4648354c8ce4c4dbe7cf2da75f764b0640ff5edb
aneerati <aneerati2004@gmail.com>
2023-09-12 17:38:45 -0400

    separated functions

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Sports-Stats
+# Sports-Stats-API
+
+Sports Stats is a simple HTTP RESTful API for getting quick scores on National Footbal League (NFL) games.
--- src/nfl.py
+++ src/nfl.py
@@ -2,21 +2,33 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def get_nfl_stats():
+def nfl_week():
     html_text = requests.get(
         'https://www.pro-football-reference.com/boxscores/').text
-
-    # Grab the year and week title
     soup = BeautifulSoup(html_text, "lxml")
+
+    # Scrape year and week title
     week_title = soup.find("div", class_="section_heading")
     week_title = str(week_title)
     week_title = week_title[33:-11]
 
-    # Scrape the game dataes
+
+def nfl_dates():
+    html_text = requests.get(
+        'https://www.pro-football-reference.com/boxscores/').text
+    soup = BeautifulSoup(html_text, "lxml")
+
+    # Scrape the game dates
     game_dates = soup.find_all("tr", class_="date")
     dates = []
     for date in game_dates:
         dates.append(date.text)
+
+
+def nfl_winners():
+    html_text = requests.get(
+        'https://www.pro-football-reference.com/boxscores/').text
+    soup = BeautifulSoup(html_text, "lxml")
 
     # Scrape the winners
     game_winners = soup.find_all("tr", class_="winner")
@@ -24,14 +36,22 @@ def get_nfl_stats():
     for winner in game_winners:
         winners.append(winner.a.text)
 
+
+def nfl_losers():
+    html_text = requests.get(
+        'https://www.pro-football-reference.com/boxscores/').text
+    soup = BeautifulSoup(html_text, "lxml")
+
     # Scrape the losers
     game_losers = soup.find_all("tr", class_="loser")
     losers = []
     for loser in game_losers:
         losers.append(loser.a.text)
 
-    # Present Info
 
-    print(f"Statistics for {week_title}:")
-    for i in range(len(dates)):
-        print(f"\t{dates[i]} - {winners[i]} defeated {losers[i]}")
+"""""
+# Present Info
+print(f"Statistics for {week_title}:")
+for i in range(len(dates)):
+    print(f"\t{dates[i]} - {winners[i]} defeated {losers[i]}")
+"""
