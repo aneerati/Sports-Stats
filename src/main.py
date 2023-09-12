53da370cfa3ed490e868ceff45c92a1c6a04ed74
aneerati <aneerati2004@gmail.com>
2023-09-12 15:55:17 -0400

    scrapped essential data andorganized

--- src/main.py
+++ src/main.py
@@ -3,4 +3,28 @@ import requests
 
 html_text = requests.get(
     'https://www.pro-football-reference.com/boxscores/').text
-print(html_text)
+
+soup = BeautifulSoup(html_text, "lxml")
+week_title = soup.find("div", class_="section_heading")
+
+week_title = str(week_title)
+print(week_title)
+
+game_dates = soup.find_all("tr", class_="date")
+dates = []
+for date in game_dates:
+    dates.append(date.text)
+print(dates)
+
+game_winners = soup.find_all("tr", class_="winner")
+winners = []
+for winner in game_winners:
+    winners.append(winner.a.text)
+print(winners)
+
+game_losers = soup.find_all("tr", class_="loser")
+losers = []
+for loser in game_losers:
+    losers.append(loser.a.text)
+print()
+print(losers)
