df9e149b691a526573f574b534c0c2f368c50a30
aneerati <aneerati2004@gmail.com>
2023-09-12 12:55:46 -0400

    yay

--- src/main.py
+++ src/main.py
@@ -1,8 +1,2 @@
 from bs4 import BeautifulSoup
-
-
-with open('src/home.html', 'r') as html_file:
-    content = html_file.read()
-
-    soup = BeautifulSoup(content, 'lxml')
-    print(soup.prettify())
+import requests
